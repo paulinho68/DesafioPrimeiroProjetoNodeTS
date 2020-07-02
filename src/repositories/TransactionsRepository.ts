ce120d292f9902c54a8eb162e57f9345954d26e6
Paulo Vitor <paulovitor2123@gmail.com>
2020-07-02 02:33:36 -0300

    First Commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    this.transactions.map(item => {
+      if(item.type === "income"){
+        income += item.value;
+        total += item.value;
+      }else{
+        outcome += item.value;
+        total -= item.value;
+      }
+    })
+
+    const balance = {income,outcome,total}
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(title: string, value: number, type: 'income' | 'outcome' ): Transaction {
+    const transaction = new Transaction({
+      title: title,
+      value: value,
+      type: type
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
